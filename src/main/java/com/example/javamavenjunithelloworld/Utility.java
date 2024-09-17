dd12c0d72581129ea8544fdc302b5472ee957830
Madhu Krishna <21eg105j19@anurag.edu.in>
2024-09-17 19:25:49 +0530

    madhuk0001 - Wrote the code for find the given number is even or odd

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,23 @@
+// Check if the given number is odd or even
+import java.util.Scanner;
+
+public class Utility {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+        
+        
+        scanner.close();
+    }
+}
